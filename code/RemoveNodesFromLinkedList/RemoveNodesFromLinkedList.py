19910ca85cd6115bfca6874cae0f9f9175de8a1d
Logan Choi <kwonhee1023@gmail.com>
2025-09-25 09:36:58 -0700

    Another Leetcode Problem Solved

--- code/RemoveNodesFromLinkedList/RemoveNodesFromLinkedList.py
+++ code/RemoveNodesFromLinkedList/RemoveNodesFromLinkedList.py
@@ -5,23 +5,18 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        validStack = [head.val]
+        validStack = [head]
         i = 0
         head = head.next
         while head:
-            if head.val <= validStack[-1]:
-                validStack.append(head.val)
+            if head.val <= validStack[-1].val:
+                validStack.append(head)
             else:
-                while validStack and validStack[-1] < head.val:
+                while validStack and validStack[-1].val < head.val:
                     validStack.pop()
-                validStack.append(head.val)
+                if validStack:
+                    validStack[-1].next = head
+                validStack.append(head)
             head = head.next
 
-        newHead = ListNode()
-        ans = newHead
-        for val in validStack:
-            newNode = ListNode(val)
-            newHead.next = newNode
-            newHead = newHead.next
-        
-        return ans.next
+        return validStack[0]
